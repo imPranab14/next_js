a0856b543e099cbfa3ac08759b2770fa0fd8179d
Pranab Quila <pranab.quila@gmail.com>
2025-07-31 22:57:38 +0530

    replace user page with input form using react-hook-form

--- src/app/user/page.js
+++ src/app/user/page.js
@@ -1,7 +1,59 @@
+"use client";
+import { useForm } from "react-hook-form";
+
 export default function index() {
-    return (
-        <h1 style={{ background: 'blue', color: 'white' }}>
-            This is my user page.
-        </h1>
-    );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  function onSubmit(value) {
+    console.log(value);
+  }
+
+  return (
+    <>
+      <p>input form</p>
+      <form onSubmit={handleSubmit(onSubmit)} className="grid w-xl gap-3 pl-2">
+        <input
+          {...register("name")}
+          type="text"
+          placeholder="Enter Full Name"
+        />
+        <input
+          {...register("email")}
+          type="email"
+          placeholder="Enter Your Email"
+        />
+        <input
+          {...register("password")}
+          type="password"
+          placeholder="Enter Your Password"
+        />
+        <input
+          {...register("age")}
+          type="number"
+          placeholder="Enter Your Age"
+        />
+        <div className="flex gap-1">
+          <input {...register("male")} type="radio" />
+          <label>Male</label>
+          <input {...register("female")} type="radio" />
+          <label>female</label>
+        </div>
+        <div className="flex gap-1">
+          <div className="flex gap-1">
+            <input type="checkbox" {...register("react")} />
+            <label>React JS</label>
+          </div>
+          <div className="flex gap-1">
+            <input type="checkbox" {...register("node")} />
+            <label>Node JS</label>
+          </div>
+        </div>
+        <input type="submit" />
+      </form>
+    </>
+  );
 }
