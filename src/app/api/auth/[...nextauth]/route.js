ac40309199887f12d4c1fc70ad242926cd78e255
imPranab14 <pranab.quila@gmail.com>
2025-07-31 12:39:48 +0530

    implement GitHub authentication, and enhance Home page UI

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,15 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
-export const authOptions = {
+const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: 'Ov23liqG6sv3sbwV5MoO',
       clientSecret:'560bd4f6ec2c4a2e37397ce1bfc8d53c499d3e19',
     }),
-    // ...add more providers here
   ],
 }
 
-export default NextAuth(authOptions)
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,31 @@
-'use client';
-
+"use client";
 import Image from "next/image";
-import { useSession,signIn } from 'next-auth/react';
+import { useSession, signIn,signOut } from "next-auth/react";
 
 export default function Home() {
-  const session =useSession()
-
-
+  const session = useSession();
   console.log(session);
-  if(session.data === null){
-    return <button onClick={signIn}>SignIN</button>
+  if (session.data === null) {
+    return (
+      <button
+        onClick={signIn}
+        className="px-4 py-1 rounded-2xl bg-blue-400 m-3"
+      >
+        Sign IN
+      </button>
+    );
   }
-
+  //Git Hub Login User Info
   console.log(session);
   return (
-    <p>
-      Home Page
-    </p>
+    <>
+      <div className="flex justify-between items-center bg-blue-100 px-6 py-3">
+        <p>Home Page</p>
+        <div className="flex items-center gap-2">
+        <p>{session.data?.user.name??"NA"}</p>
+        <button className="bg-red-400 px-3 py-2 rounded-xl text-sm" onClick={signOut}>Logout</button>
+        </div>
+      </div>
+    </>
   );
 }
