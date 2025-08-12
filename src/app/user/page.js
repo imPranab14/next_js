e855be22c6ae8d296b237d6f869d12867307bb05
Pranab Quila <pranab.quila@gmail.com>
2025-08-13 00:27:31 +0530

    Enhance user input form with validation and email availability check

--- src/app/user/page.js
+++ src/app/user/page.js
@@ -1,4 +1,5 @@
 "use client";
+import axios from "axios";
 import { useForm } from "react-hook-form";
 
 export default function index() {
@@ -12,25 +13,44 @@ export default function index() {
     console.log(value);
   }
 
+  console.log("error", errors);
+
   return (
     <>
       <p>input form</p>
       <form onSubmit={handleSubmit(onSubmit)} className="grid w-xl gap-3 pl-2">
         <input
-          {...register("name")}
+          {...register("name", { required: true })}
           type="text"
           placeholder="Enter Full Name"
         />
+        {errors.name && <p>name is required</p>}
         <input
-          {...register("email")}
+          {...register("email", {
+            required: { value: true, message: "email is required" },
+            validate: async function (value) {
+              try {
+                const response = await axios.post(
+                  `http://localhost:8085/check-email?email=${value}`
+                );
+                console.log("response", response);
+                if (response.status === 200) return "emil id available";
+                else return "emil if already exits";
+              } catch {
+                return "Error checking email";
+              }
+            },
+          })}
           type="email"
           placeholder="Enter Your Email"
         />
+        {errors.email && errors.email.message}
         <input
           {...register("password")}
           type="password"
           placeholder="Enter Your Password"
         />
+        <button>Show</button>
         <input
           {...register("age")}
           type="number"
@@ -42,6 +62,7 @@ export default function index() {
           <input {...register("female")} type="radio" />
           <label>female</label>
         </div>
+
         <div className="flex gap-1">
           <div className="flex gap-1">
             <input type="checkbox" {...register("react")} />
